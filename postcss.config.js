dfddc97e91e3fe4098effe43729631379875d85d
Bonajo <m.bonajo@fontys.nl>
2022-01-27 15:43:37 +0100

    build: add support for nested css selectors

--- postcss.config.js
+++ postcss.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
     plugins: [
         require('autoprefixer'),
-        require('postcss-nested'),
+        require('tailwindcss/nesting'),
         require('tailwindcss')(path.resolve(__dirname, 'tailwind.config.js'),),
     ]
 };
