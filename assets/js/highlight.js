7f9c99b35d29da3bfe953bb6c36a211935eec31f
Bonajo <m.bonajo@fontys.nl>
2022-01-31 16:43:26 +0100

    feat: add support for code copy

--- assets/js/highlight.js
+++ assets/js/highlight.js
@@ -1,10 +1,11 @@
 import hljs from 'highlight.js/lib/core';
-
 import javascript from 'highlight.js/lib/languages/javascript';
 import json from 'highlight.js/lib/languages/json';
 import bash from 'highlight.js/lib/languages/bash';
 import xml from 'highlight.js/lib/languages/xml';
 import java from 'highlight.js/lib/languages/java';
+
+import Clipboard from 'clipboard';
 
 hljs.registerLanguage('javascript', javascript);
 hljs.registerLanguage('json', json);
@@ -15,6 +16,20 @@ hljs.registerLanguage('java', java);
 
 document.addEventListener('DOMContentLoaded', () => {
   document.querySelectorAll('pre code:not(.language-mermaid)').forEach((block) => {
+    block.insertAdjacentHTML('beforebegin', '<button class="code-copy"><svg viewBox="0 0 24 24" width="24" height="24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" class="css-i6dzq1"><path d="M16 4h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h2"></path><rect x="8" y="2" width="8" height="4" rx="1" ry="1"></rect></svg></button>');
     hljs.highlightElement(block);
+  });
+
+  const clipboard = new Clipboard('.code-copy', {
+
+    target: function(trigger) {
+      console.log("trigger");
+      return trigger.nextElementSibling;
+    },
+  
+  });
+  
+  clipboard.on('success', function(e) {
+      e.clearSelection();
   });
 });
