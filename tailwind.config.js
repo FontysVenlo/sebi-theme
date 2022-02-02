c101b573d6fd48b37bd1b438b8919aa04282db16
Bonajo <m.bonajo@fontys.nl>
2022-02-02 11:23:51 +0100

    fix: tailwind was looking at wrong assets directory
    tailwind was looking at the assets directory of the website using this theme
    instead of the assets directory of the theme itself

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  content: [path.resolve(__dirname, 'layouts/**/*.{html,js}'), "./public/**/*.html", "./assets/**/*.js"],
+  content: [path.resolve(__dirname, 'layouts/**/*.{html,js}'), "./public/**/*.html", path.resolve(__dirname, 'assets/**/*.{ts,js}')],
   safelist: [
     'code-copy'
   ],
